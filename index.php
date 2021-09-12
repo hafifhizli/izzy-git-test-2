75978c9f7c5d374d039d026f054a9a587acb5839
Hafif Hizli <hafifhizli@gmail.com>
2021-09-12 22:18:37 +0700

    mengubah file php 2

--- index.php
+++ index.php
@@ -9,5 +9,13 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti quia eaque cupiditate praesentium, at unde omnis recusandae sint numquam et!</p>
+    <ul>
+        <li>
+            satu
+        </li>
+        <li>
+            dua
+        </li>
+    </ul>
 </body>
 </html>
